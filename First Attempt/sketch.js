743f076e31d9c12778dd952ee21fa58bc5aa5f12
faith-oops <58151755+faith-oops@users.noreply.github.com>
2024-11-12 13:25:13 -0800

    scatter plot tutorial from https://editor.p5js.org/allison.parrish/sketches/ry9wlx46b

--- First Attempt/sketch.js
+++ First Attempt/sketch.js
@@ -1,7 +1,47 @@
-function setup() {
-  createCanvas(windowWidth, windowHeight);
+/** First full attempt at data viz within p5.js; declares 'data' as
+ information in the table, function ColValsMinMax returns the values for
+ specific columns, 
+ **/
+let data;
+
+function preload() {
+  data = loadTable('PRSA-adapted-aparrish.csv', 'csv', 'header');
 }
 
-function draw() {
-  background(220);
+function colValsMinMax(tab, colName) {
+  let vals = data.getColumn(colName);
+  let obj = {
+    values: vals, 
+    min: min(vals), 
+    max: max(vals),
+  }
+  return obj;
 }
+
+function setup() {
+  createCanvas(windowWidth, windowHeight);
+  console.log(data.getRowCount());
+  console.log(data.columns);
+
+  background(50);
+  stroke(255);
+
+  let pm = colValsMinMax(data, "pm2.5");
+  console.log(pm.min);
+  console.log(pm.max);
+
+  let iws = colValsMinMax (data, "Iws");
+  console.log(iws.min);
+  console.log(iws.max);
+
+  for (var i = 0; i < data.getRowCount(); i++) {
+    stroke (255, 128, 128);
+    let xpos = map(pm.values[i], pm.min, pm.max, 0, width);
+    let ypos = map(iws.values[i], iws.min, iws.max, height, 0);
+
+    point(xpos, ypos);
+  }
+
+}
+
+
